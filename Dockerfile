c398d1f4dc4f832c7a5c6bca4aae94467ca50a03
Ampersander <romain-b49460@hotmail.com>
2022-09-22 15:00:21 +0200

    ttest

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 FROM node:16 AS builder
 WORKDIR "/app"
 COPY . .
@@ -12,18 +11,4 @@ COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/views ./views
 COPY --from=builder /app/node_modules ./node_modules
-=======
-FROM node:16 AS builder
-WORKDIR "/app"
-COPY . .
-RUN npm i
-RUN npm run build
-RUN npm prune --production
-FROM node:16-alpine AS production
-WORKDIR "/app"
-COPY --from=builder /app/package.json ./package.json
-COPY --from=builder /app/dist ./dist
-COPY --from=builder /app/views ./views
-COPY --from=builder /app/node_modules ./node_modules
->>>>>>> a5868b2a54ec50095aa6c873a92b7cc5069d039d
 CMD [ "sh", "-c", "npm run start:prod"]
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HttpService } from 'nestjs-http-promise';
 import { Injectable } from '@nestjs/common';
 import Joke from './joke';
@@ -25,31 +24,3 @@ export class AppService {
     return 'Hello World!';
   }
 }
-=======
-import { HttpService } from 'nestjs-http-promise';
-import { Injectable } from '@nestjs/common';
-import Joke from './joke';
-
-@Injectable()
-export class AppService {
-  constructor(private readonly httpService: HttpService) {}
-
-  async getJoke(): Promise<Joke> {
-    const response = await this.httpService.get(
-      process.env.JOKE_URL || 'https://v2.jokeapi.dev/joke/Any',
-      {
-        headers: {
-          accept: 'application/json',
-        },
-      },
-    );
-    const data = response.data;
-
-    return data;
-  }
-
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
->>>>>>> a5868b2a54ec50095aa6c873a92b7cc5069d039d
